1971ce92fa63d1938762162caae31af64b996a50
lyrnivy <60031000+lyrnivy@users.noreply.github.com>
2026-02-23 18:29:42 +0800

    corrected again gallery

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import '../css/base.css'
-import '../css/gallery.css'
-import '../css/layout.css'
-import '../css/cards.css'
-import { initGallery } from './gallery.js'
-import { initCards } from './cards.js'
+import './css/base.css'
+import './css/gallery.css'
+import './css/layout.css'
+import './css/cards.css'
+import { initGallery } from './js/gallery.js'
+import { initCards } from './js/cards.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   initGallery();
