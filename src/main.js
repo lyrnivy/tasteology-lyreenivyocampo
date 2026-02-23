3f466e54860d088d2760d462e346210e668b7f49
lyrnivy <60031000+lyrnivy@users.noreply.github.com>
2026-02-23 18:22:42 +0800

    bring back correct path

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import './css/base.css'
-import './css/gallery.css'
-import './css/layout.css'
-import './css/cards.css'
+import '../css/base.css'
+import '../css/gallery.css'
+import '../css/layout.css'
+import '../css/cards.css'
 import { initGallery } from './gallery.js'
 import { initCards } from './cards.js'
 
