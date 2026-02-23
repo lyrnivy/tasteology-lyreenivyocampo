57ce8f277b18f86a9b50a50569ede723af823e79
lyrnivy <60031000+lyrnivy@users.noreply.github.com>
2026-02-23 17:59:25 +0800

    try to move the main

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import '../css/base.css'
-import '../css/gallery.css'
-import '../css/layout.css'
-import '../css/cards.css'
+import './css/base.css'
+import './css/gallery.css'
+import './css/layout.css'
+import './css/cards.css'
 import { initGallery } from './gallery.js'
 import { initCards } from './cards.js'
 
-// Initialize your functions
 document.addEventListener('DOMContentLoaded', () => {
   initGallery();
   initCards();
